68ebd8780678ea96eb14e75b8922019776bdd463
Nika <shvangiradze1010@gmail.com>
2025-12-20 12:15:28 -0800

    install primeng

--- tbc-cross-collab/tbc-cross-collab/src/app/features/event-hub/event-hub.ts
+++ tbc-cross-collab/tbc-cross-collab/src/app/features/event-hub/event-hub.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-event-hub',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './event-hub.html',
   styleUrl: './event-hub.scss',
 })
