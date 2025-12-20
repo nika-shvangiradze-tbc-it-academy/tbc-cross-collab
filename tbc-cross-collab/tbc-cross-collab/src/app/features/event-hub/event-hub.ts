0bb6c63fb64497dbceccc640b4b604c50d56ba5f
Nika <shvangiradze1010@gmail.com>
2025-12-20 15:14:42 -0800

    feat(ts)create header/live ui of status

--- tbc-cross-collab/tbc-cross-collab/src/app/features/event-hub/event-hub.ts
+++ tbc-cross-collab/tbc-cross-collab/src/app/features/event-hub/event-hub.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Header } from '../../shared/components/header/header';
 
 @Component({
   selector: 'app-event-hub',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, Header],
   templateUrl: './event-hub.html',
   styleUrl: './event-hub.scss',
 })
